e340b008a2ba77a40287398e1731f8baa8b8b20d
Taniya0501 <iit2019054@iiita.ac.in>
2021-07-15 00:11:32 -0700

    final card css added and refactoring

--- scoreKeeper.js
+++ scoreKeeper.js
@@ -1,41 +1,41 @@
-const p1Display = document.querySelector('#p1Display');
-const p2Display = document.querySelector('#p2Display');
-const p1Button = document.querySelector('#p1Button');
-const p2Button = document.querySelector('#p2Button');
+const p1 = {
+	score: 0,
+	display: document.querySelector('#p1Display'),
+	button: document.querySelector('#p1Button')
+};
+
+const p2 = {
+	score: 0,
+	display: document.querySelector('#p2Display'),
+	button: document.querySelector('#p2Button')
+};
+
 const resetButton = document.querySelector('#reset');
 const winningScoreSelect = document.querySelector('#playTo');
 
-let p1Score = 0;
-let p2Score = 0;
 let winningScore = 3;
 let isGameOver = false;
 
-p1Button.addEventListener('click', function() {
+function updateScore(player,opponent) {
 	if(!isGameOver) {
-		p1Score += 1;
-		if(p1Score === winningScore) {
+		player.score += 1;
+		if(player.score === winningScore) {
 			isGameOver = true;
-			p1Display.classList.add('winner');
-			p2Display.classList.add('loser');
-			p1Button.disabled = true;
-			p2Button.disabled = true;
+			player.display.classList.add('winner');
+			opponent.display.classList.add('loser');
+			player.button.disabled = true;
+			opponent.button.disabled = true;
 		}
-		p1Display.textContent = p1Score;
+		player.display.textContent = player.score;
 	}
+}
+
+p1.button.addEventListener('click', function() {
+	updateScore(p1,p2);
 })
 
 p2Button.addEventListener('click', function() {
-	if(!isGameOver) {
-		p2Score += 1;
-		if(p2Score === winningScore) {
-			isGameOver = true;
-			p2Display.classList.add('winner');
-			p1Display.classList.add('loser');
-			p1Button.disabled = true;
-			p2Button.disabled = true;
-		}
-		p2Display.textContent = p2Score;
-	}
+	updateScore(p2,p1);
 })
 
 resetButton.addEventListener('click', reset)
@@ -46,13 +46,11 @@ winningScoreSelect.addEventListener('change', function() {
 })
 
 function reset() {
-	p1Score = 0;
-	p2Score = 0;
 	isGameOver = false;
-	p1Display.textContent = p1Score;
-	p2Display.textContent = p2Score;
-	p1Button.disabled = false;
-	p2Button.disabled = false;
-	p1Display.classList.remove('winner','loser');
-	p2Display.classList.remove('winner','loser');
+	for(p of [p1,p2]) {
+		p.score = 0;
+		p.display.textContent = 0;
+		p.button.disabled = false;
+		p.display.classList.remove('winner','loser');
+	}
 }
