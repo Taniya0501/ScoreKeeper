4f66c2ba6f7137b454f3232cdfcae712894f7990
Taniya0501 <iit2019054@iiita.ac.in>
2021-07-14 22:39:38 -0700

    color changing property added

--- scoreKeeper.js
+++ scoreKeeper.js
@@ -2,16 +2,21 @@ const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
 const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
+const resetButton = document.querySelector('#reset');
+const winningScoreSelect = document.querySelector('#playTo');
 
 let p1Score = 0;
 let p2Score = 0;
-let winningScore = 5;
+let winningScore = 3;
 let isGameOver = false;
+
 p1Button.addEventListener('click', function() {
 	if(!isGameOver) {
 		p1Score += 1;
 		if(p1Score === winningScore) {
 			isGameOver = true;
+			p1Display.classList.add('winner');
+			p2Display.classList.add('loser');
 		}
 		p1Display.textContent = p1Score;
 	}
@@ -22,7 +27,26 @@ p2Button.addEventListener('click', function() {
 		p2Score += 1;
 		if(p2Score === winningScore) {
 			isGameOver = true;
+			p2Display.classList.add('winner');
+			p1Display.classList.add('loser');
 		}
 		p2Display.textContent = p2Score;
 	}
 })
+
+resetButton.addEventListener('click', reset)
+
+winningScoreSelect.addEventListener('change', function() {
+	winningScore = parseInt(this.value);
+	reset();
+})
+
+function reset() {
+	p1Score = 0;
+	p2Score = 0;
+	isGameOver = false;
+	p1Display.textContent = p1Score;
+	p2Display.textContent = p2Score;
+	p1Display.classList.remove('winner','loser');
+	p2Display.classList.remove('winner','loser');
+}
